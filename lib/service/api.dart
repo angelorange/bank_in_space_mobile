be2b4dcee472e2a9191209fdc03cff744144efb4
angelorange <acostalarangeira@gmail.com>
2021-09-04 18:03:27 -0300

    feat: add api and login model

---
+++ lib/service/api.dart
@@ -0,0 +1,19 @@
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import '../model/login_model.dart';
+
+class APIService {
+  Future<LoginResponseModel> login(LoginRequestModel requestModel) async {
+    String url = "bck-api-test.herokuapp.com/users/sign_in";
+
+    final response =
+        await http.post(Uri.parse(url), body: requestModel.toJson());
+    if (response.statusCode == 200 || response.statusCode == 400) {
+      return LoginResponseModel.fromJson(
+        json.decode(response.body),
+      );
+    } else {
+      throw Exception('Failed to load data!');
+    }
+  }
+}
